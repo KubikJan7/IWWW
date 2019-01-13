8042e17a9263861ca4b49cd15bb686c846ceeab0
st52531 <43739550+st52531@users.noreply.github.com>
2019-01-13 20:24:21 +0100

    empty cart design added

--- term-work/code/term_work/page/cart.php
+++ term-work/code/term_work/page/cart.php
@@ -1,2 +1,5 @@
-<h1>This is Cart</h1>
-This page is included in the index.php
+<section id = cart-section>
+    <div><h1>Košík je prázdný, přidejte do něj zboží.</h1></div>
+    <div id = "big-cart-img"><i class="fa fa-shopping-cart"></i></div>
+    <a class="button" href="<?= BASE_URL ?>"><i class="fa fa-shopping-cart"></i> Jít nakupovat</a>
+</section>
